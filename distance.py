d5202e4bb92537dc3268f3c29ee7256ed6c1b8b8
Alex <alexcn711@gmail.com>
2021-10-24 18:05:29 -0600

    added pycharm

--- distance.py
+++ distance.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+
 def cosine_distance(x, y):
     print("Calculating distance...")
     x_transpose = np.transpose(x)
     numerator = x_transpose * y
     denominator = np.linalg.norm(x) * np.linalg.norm(y)
     return numerator / denominator
+
 
 print("Hello cosine distance function")
 x = np.array([1, 2, 3])
